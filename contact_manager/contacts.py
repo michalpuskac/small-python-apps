85b5080fee446719efcde21b13b8239edb4a0964
Michal Puškáč <133041741+michalpuskac@users.noreply.github.com>
2024-11-27 09:32:01 +0100

    feat: Add menu function outside of main loop - Add user choice validation - Add error handling durng save a load

--- contact_manager/contacts.py
+++ contact_manager/contacts.py
@@ -9,18 +9,38 @@ class ContactManager:
 
 
     def _load_contacts(self):
-        """Read contacts from JSON file to dictionary."""
-        if os.path.exists(self.file_path):
-            with open(self.file_path, "r") as file:
-                return json.load(file)
-        return {} #empty dictionary if JSON file does not exist
+        """Read contacts from JSON file into dictionary with error hangdling."""
+        try:
+            if os.path.exists(self.file_path):
+                with open(self.file_path, "r") as file:
+                    return json.load(file)
+            else:
+                # If file doesn't exist, initialize an empty contacts dictionary
+                print(f"Info: Contacts file '{self.file_path}' not found. Starting with an empty contact list.")
+                return {}
+
+        except json.JSONDecodeError:
+            print(f"Error: The contacts file '{self.file_path}' is corrupted and cannot be loaded. Starting with an empty contact list.")
+            return {}
+        except FileNotFoundError:
+            print(f"Error: The specified file '{self.file_path}' could not be found.")
+            return {}
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            return {}
 
 
     def _save_contacts(self):
         """Save contacts to JSON file."""
-        os.makedirs(os.path.dirname(self.file_path), exist_ok= True)
-        with open(self.file_path, "w") as file:
-            json.dump(self.contacts, file, indent=4)
+        try:
+            os.makedirs(os.path.dirname(self.file_path), exist_ok= True)
+            with open(self.file_path, "w") as file:
+                json.dump(self.contacts, file, indent=4)
+
+        except FileNotFoundError:
+            print(f"Error: The directory for '{self.file_path}' does not exist and could not be created.")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
 
 
     def is_valid_number(self, number):
@@ -29,11 +49,10 @@ class ContactManager:
     def add_new_contact(self, name, number):
         """Adds ne contact to file"""
         if not self.is_valid_number(number):
-            print("Invalid phone number. It must be 9-15 digits and may start with +.")
+            print("Invalid phone number. It must be 9-15 digits and may start with +.\n")
             return
-        
         if name.lower() in self.contacts:
-            print(f"Contact with name {name} exists!")
+            print(f"Contact with name {name} already exists!")
         else:
             self.contacts[name.lower()] = number
             self._save_contacts() #automatic save
@@ -52,7 +71,7 @@ class ContactManager:
 
     def search_contact(self, name):
         """Search contact by name"""
-        return self.contacts.get(name.lower(), "Contact not Found")
+        return self.contacts.get(name.lower(), "Contact not Found\n")
 
 
     def delete_contact(self,name):
@@ -62,7 +81,7 @@ class ContactManager:
             self._save_contacts() #Auto save after deleting contact
             print(f"Contact {name} was deleted.")
         else:
-            print(f"Contact {name} not found.")
+            print(f"Contact {name} not found.\n")
 
     def export_to_csv(self, contacts, file_path = "contact_manager/data/contacts_export.csv"):
         """Exports all contacts to CSV file."""
@@ -74,4 +93,4 @@ class ContactManager:
                     writer.writerow([name, number])
                 print(f"Contacts where successfully exported to {file_path}.")
         except Exception as e:
-            print(f"Export to CSV was unsuccessfull : {e}")
+            print(f"Export to CSV was unsuccessfull : {e}\n")
--- contact_manager/main.py
+++ contact_manager/main.py
@@ -1,27 +1,32 @@
 from contacts import ContactManager
 
+def menu():
+    print("\n---Contact Manager---")
+    print("1.---Add new contact---")
+    print("2.---Show all contacts---")
+    print("3.---Search contact---")
+    print("4.---Delete contact---")
+    print("5.---Export all contacts to CSV---")
+    print("---For exit type 'EXIT'---\n")
 
 def main():
     manager = ContactManager()
-    4
-    while True:
-        print("\n---Contact Manager---")
-        print("1.---Add new contact---")
-        print("2.---Show all contacts---")
-        print("3.---Search contact---")
-        print("4.---Delete contact---")
-        print("5.---Export all contacts to CSV---")
-        print("---For exit type 'EXIT'---\n")
 
-        choice = input("What you want to do. Choose 1, 2, 3, 4 or 5: ")
+    menu()
+    while True:
+        choice = input("**What you want to do. Choose 1, 2, 3, 4 or 5**: ")
+        if choice not in {"1", "2", "3", "4", "5", "exit"}:
+            print("Invalid input. Try again with choice 1 to 5 or 'exit'\n")
+            continue
         
         if choice == "1":
             name = input("Enter name of contact: ").strip()
             number = input("Enter name of contact: ").strip()
             manager.add_new_contact(name, number)
-            
+
         elif choice == "2":
             manager.show_all_contacts()
+            menu()
 
         elif choice == "3":
             name = input("Enter name of contact you want find: ").strip()
@@ -38,9 +43,6 @@ def main():
         elif choice.lower() == "exit":
             print("Program terminated.")
             break
-        else:
-            print("\nInvalid input. Try again with choice 1 to 5 or 'exit'")
-        
 
 if __name__ == "__main__":
     main()
